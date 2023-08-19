4ee7ebb27a42cf22e754599ee32b2ceb4368bce2
Ishuboi07 <123348813+Ishuboi07@users.noreply.github.com>
2023-08-19 19:21:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,14 +9,39 @@ buttons.forEach(button => {
     });
 });
 
-calculateButton.addEventListener("click", () => {
+// Function to perform calculation
+function calculate() {
     try {
         display.value = eval(display.value);
     } catch (error) {
         display.value = "Error";
     }
+}
+
+// Function to clear display
+function clearDisplay() {
+    display.value = "";
+}
+
+// Event listener for keyboard key press
+document.addEventListener("keydown", (event) => {
+    const key = event.key;
+    if (/[\d+\-*/.=]/.test(key)) {
+        handleKeyPress(key);
+    }
 });
 
-clearButton.addEventListener("click", () => {
-    display.value = "";
-});
+// Function to handle key press
+function handleKeyPress(key) {
+    if (key === "Enter") {
+        calculate();
+    } else if (key === "Escape") {
+        clearDisplay();
+    } else {
+        display.value += key;
+    }
+}
+
+// Attach click event listeners to buttons
+calculateButton.addEventListener("click", calculate);
+clearButton.addEventListener("click", clearDisplay);
